eb60953bfb2a003e67f40550710d0398465f18db
pinkra <jobezic@gmail.com>
2020-06-05 16:07:20 +0200

    Fix round trip spec

--- spec/ryanair_scraper_spec.rb
+++ spec/ryanair_scraper_spec.rb
@@ -95,19 +95,24 @@ RSpec.describe RyanairScraper do
       end
 
       it 'returns valid flights for the specific origin, destination and dates' do
-        response = command.call.first
+        response = command.call
 
         expect(response).to eq(
-          origin: 'AGP',
-          destination: 'BLQ',
-          dates: [
+          [
             {
-              date_out: '2020-07-01T00:00:00.000',
-              flights: [{ fares_left: -1, fares: [{ amount: 59.52 }] }]
+              origin: 'AGP',
+              destination: 'BLQ',
+              dates: [
+                {
+                  date_out: '2020-07-03T00:00:00.000',
+                  flights: [{ fares_left: 4, fares: [{ amount: 75.99 }] }]
+                }
+              ]
             },
             {
-              date_out: '2020-07-03T00:00:00.000',
-              flights: [{ fares_left: 4, fares: [{ amount: 75.99 }] }]
+              origin: 'BLQ',
+              destination: 'AGP',
+              dates: []
             }
           ]
         )
